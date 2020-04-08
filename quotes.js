9abca10625132331baa618f7c76507061f848953
Jon Langlois <jonathan-langlois@live.ca>
2020-04-08 12:29:44 -0700

    error handling and async

--- quotes.js
+++ quotes.js
@@ -1,16 +1,21 @@
-window.onload = () => {
-  fetch('https://quotes.rest/qod')
-  .then(body => body.text())
-  .then(body => {
-    
-    const resJSON = JSON.parse(body) 
+const quotes = async () => {
+  try {
+    const body = await fetch('https://quotes.rest/qod')
+    const bodyText = await body.text();
+    const resJSON = JSON.parse(bodyText) 
     const quoteInfo = resJSON.contents.quotes[0];
     const quoteText = quoteInfo.quote;
     const quoteAuthor = quoteInfo.author;
-
+  
     document.getElementById("quote").textContent = quoteText
     document.getElementById("author").textContent = `- ${quoteAuthor}`
-  });
+  }
+  catch (err) {
+    console.error(err);
+  }
+}
 
-  startTime()
+window.onload = () => {
+  quotes();
+  startTime();
 }
--- time.js
+++ time.js
@@ -1,15 +1,15 @@
-
+//Begin clock
 function startTime() {
   const today = new Date();
   const hour = today.getHours();
   let min = today.getMinutes();
-  min = checkTime(min);
+  min = padTime(min);
   document.getElementById('time').innerHTML =
   `${hour} : ${min}`;
   const t = setTimeout(startTime, 1000);
 }
 
-function checkTime(i) {
+function padTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
 }
