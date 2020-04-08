18802faf9165fb4c27f6c85b2de1608b5e75c4ff
Jon Langlois <jonathan-langlois@live.ca>
2020-04-08 11:28:51 -0700

    add qoute of the day and search

---
+++ quotes.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+  fetch('https://quotes.rest/qod')
+  .then(body => body.text())
+  .then(body => {
+    
+    const resJSON = JSON.parse(body) 
+    const quoteInfo = resJSON.contents.quotes[0];
+    const quoteText = quoteInfo.quote;
+    const quoteAuthor = quoteInfo.author;
+
+    document.getElementById("quote").textContent = quoteText
+    document.getElementById("author").textContent = `- ${quoteAuthor}`
+  })
+}
